61acb45774033c8f2b5885f45ade8a6397dc3c44
architolk <msj@xs4all.nl>
2022-03-30 20:01:01 +0200

    Update

--- src/main/java/nl/architolk/csv2ttl/CSV2TTL.java
+++ src/main/java/nl/architolk/csv2ttl/CSV2TTL.java
@@ -1,12 +1,51 @@
 package nl.architolk.csv2ttl;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class CSV2TTL {
 
-   public static void main(String[] args) {
+  static final int KADASTRALE_GEMEENTE_CODE = 2;
+
+  private static header() {
+    return null;
+  }
+
+  private static boolean filter(String line) {
+    return line.contains(",\"LSE00\",");
+  }
+
+  private static String process(String line) {
+    String[] items = line.split(",");
+    return items[KADASTRALE_GEMEENTE_CODE];
+  }
+
+  public static void main(String[] args) {
 
     System.out.println("== CSV2TTL ==");
     System.out.println("Input: " + args[0]);
     System.out.println("Output: " + args[1]);
 
-   }
+    try(BufferedReader inputReader = new BufferedReader(new FileReader(args[0]))) {
+      try(PrintWriter outputWriter = new PrintWriter(new BufferedWriter(new FileWriter(args[1])))) {
+        String header = header();
+        if (header != null) {
+          outputWriter.println(header);
+        }
+        String line = inputReader.readLine();
+
+        while (line != null) {
+            if (filter(line)) {
+              outputWriter.println(process(line));
+            }
+            line = inputReader.readLine();
+        }
+      }
+    } catch (Exception e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+  }
 }
--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -jar "target/csv2ttl-1.0.0-jar-with-dependencies.jar" "input/input.csv" "output/output.ttl"
+java -jar "target/csv2ttl-1.0.0-jar-with-dependencies.jar" "input/lisse.csv" "output/output.ttl"
